ba241259162b5fe552b99f25723c3b23fbeb9b17
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-04 16:17:10 +0300

    Tests updated.

--- src/test/kotlin/RegionMatchingTests.kt
+++ src/test/kotlin/RegionMatchingTests.kt
@@ -1,3 +1,4 @@
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Test
 
 data class MutablePos3(var x: Int, var y: Int, var z: Int)
@@ -5,51 +6,30 @@ data class MutablePos3(var x: Int, var y: Int, var z: Int)
 class RegionMatchingTests {
     @Test
     fun `match simple inner region`() {
-        val innerRegionMin = MutablePos3(30, 70, 30)
-        val innerRegionMax = MutablePos3(60, 80, 70)
-        val outerRegionMin = MutablePos3(0, 70, 0)
-        val outerRegionMax = MutablePos3(50, 75, 44)
-
-        // @formatter:off
-        val innerPresent =
-            outerRegionMax.x > innerRegionMin.x && outerRegionMin.x < innerRegionMax.x &&
-            outerRegionMax.y > innerRegionMin.y && outerRegionMin.y < innerRegionMax.y &&
-            outerRegionMax.z > innerRegionMin.z && outerRegionMin.z < innerRegionMax.z
-        println(innerPresent)
-        assert(innerPresent)
-        // @formatter:on
+        val innerMin = MutablePos3(30, 70, 30)
+        val innerMax = MutablePos3(60, 80, 70)
+        val outerMin = MutablePos3(0, 70, 0)
+        val outerMax = MutablePos3(50, 75, 44)
+        assert(regionsIsIntersect(outerMax, outerMin, innerMax, innerMin))
     }
 
     @Test
     fun `no match simple inner region`() {
-        val innerRegionMin = MutablePos3(30, 70, 30)
-        val innerRegionMax = MutablePos3(60, 80, 70)
-        val outerRegionMin = MutablePos3(-30, 70, -30)
-        val outerRegionMax = MutablePos3(0, 70, 0)
-
-        // @formatter:off
-        val innerPresent =
-            outerRegionMax.x > innerRegionMin.x && outerRegionMin.x < innerRegionMax.x &&
-            outerRegionMax.y > innerRegionMin.y && outerRegionMin.y < innerRegionMin.y &&
-            outerRegionMax.z > innerRegionMin.z && outerRegionMin.z < innerRegionMax.z
-
-        assert(!innerPresent)
-        // @formatter:on
+        val innerMin = MutablePos3(30, 70, 30)
+        val innerMax = MutablePos3(60, 80, 70)
+        val outerMin = MutablePos3(-30, 70, -30)
+        val outerMax = MutablePos3(0, 70, 0)
+        shouldNotBe(regionsIsIntersect(outerMax, outerMin, innerMax, innerMin))
     }
 
-//    @Test
-//    suspend fun `match inner regions`() {
-//        forAll(
-//            row(MutablePos3(0, 60, 13), MutablePos3(100, 70, 90)),
-//            row(MutablePos3(1647, 60, 13), MutablePos3(9713, 70, 90)),
-//            row(MutablePos3(-130, 75, 13), MutablePos3(6, 85, 90))
-//        ) { min, max ->
-//
-//
-//
-//            assert(true)
-//        }
-//    }
+    private fun regionsIsIntersect(
+        outerRegionMax: MutablePos3,
+        outerRegionMin: MutablePos3,
+        innerRegionMax: MutablePos3,
+        innerRegionMin: MutablePos3
+    ) = outerRegionMax.x > innerRegionMin.x && outerRegionMin.x < innerRegionMax.x &&
+            outerRegionMax.y > innerRegionMin.y && outerRegionMin.y < innerRegionMax.y &&
+            outerRegionMax.z > innerRegionMin.z && outerRegionMin.z < innerRegionMax.z
 }
 
 
