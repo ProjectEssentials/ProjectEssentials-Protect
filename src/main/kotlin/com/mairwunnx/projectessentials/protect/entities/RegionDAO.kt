10be764927b1f47ca24954be52c9e65ae3249321
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-05 23:16:09 +0300

    `participants` in RegionDAO.kt default value fixed.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/entities/RegionDAO.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/entities/RegionDAO.kt
@@ -8,7 +8,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 object RegionTable : IntIdTable() {
     val name = varchar("name", 16).uniqueIndex().index()
     val creator = varchar("creator", 16).default("#server")
-    val participants = text("participants").default("[test:member]")
+    val participants = text("participants").default("[]")
     val flags = text("flags").default("[]")
     val priority = integer("priority").default(0)
     val dimension = RegionTable.integer("dimension")
