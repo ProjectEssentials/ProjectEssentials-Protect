f812b8b962e8b0d227eb00792d2ee2aa1e4c7040
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-06 19:02:05 +0300

    ProtectConfigurationModel.kt updated.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/configurations/ProtectConfigurationModel.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/configurations/ProtectConfigurationModel.kt
@@ -1,6 +1,5 @@
 package com.mairwunnx.projectessentials.protect.configurations
 
 data class ProtectConfigurationModel(
-    var regionNamesCacheSize: Int = 128,
-    var regionPositionsCacheSize: Int = 64
+    var regionCacheSize: Int = 128
 )
