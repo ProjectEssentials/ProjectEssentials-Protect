386afd66f8dc2ae97ddc9e6fd74a40e386a2b252
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-06 19:01:04 +0300

    CacheManager.kt documentation added.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/managers/CacheManager.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/managers/CacheManager.kt
@@ -1,6 +1,5 @@
 package com.mairwunnx.projectessentials.protect.managers
 
-import com.mairwunnx.projectessentials.protect.RegionPositions
 import com.mairwunnx.projectessentials.protect.configuration
 import com.mairwunnx.projectessentials.protect.dao.RegionEntity
 import com.soywiz.kds.CacheMap
@@ -8,27 +7,47 @@ import org.apache.logging.log4j.LogManager
 
 private val logger = LogManager.getLogger()
 
-internal var regionNameCache = CacheMap<String, RegionEntity>(64)
-internal var regionPositionsCache = CacheMap<RegionPositions, RegionEntity>(32)
+/**
+ * Region cache, key is a region name, value is a region entity.
+ *
+ * @since 1.0.0.
+ */
+var regionCache = CacheMap<String, RegionEntity>(64)
 
+/**
+ * Initializing cache, creating [CacheMap] instance with defining
+ * max size of cache of regions.
+ *
+ * @since 1.0.0.
+ */
 fun initializeCache() = logger.debug("Initializing cache of regions").also {
-    regionNameCache = CacheMap(validateSize(configuration.take().regionNamesCacheSize))
-    regionPositionsCache = CacheMap(validateSize(configuration.take().regionPositionsCacheSize))
+    regionCache = CacheMap(validateSize(configuration.take().regionCacheSize))
 }
 
-fun invalidateCache() = logger.warn("Invalidating cache of regions").also {
-    regionNameCache.clear().also { regionPositionsCache.clear() }
-}
+/**
+ * Cache target region, adds [RegionEntity] instance to [regionCache] map.
+ *
+ * @param region region to cache.
+ * @return cached region instance.
+ * @since 1.0.0.
+ */
+fun cache(region: RegionEntity) = synchronized(regionCache) { regionCache.put(region.name, region) }
 
-fun cache(region: () -> RegionEntity) = cache(region())
+/**
+ * Removed target region by name from cache map.
+ *
+ * @param name region to remove from cache.
+ * @return cached region instance.
+ * @since 1.0.0.
+ */
+fun invalidate(name: String) = synchronized(regionCache) { regionCache.remove(name) }
 
-fun cache(region: RegionEntity) {
-    synchronized(regionNameCache) { regionNameCache.put(region.name, region) }
-    synchronized(regionPositionsCache) {
-        with(region) {
-            regionPositionsCache.put(RegionPositions(minX, minY, minZ, maxX, maxY, maxZ), this)
-        }
-    }
+/**
+ * Invalidate all cache, it will do simply clear cache map.
+ * @since 1.0.0.
+ */
+fun invalidateAll() = logger.warn("Invalidating cache of regions").also {
+    synchronized(regionCache) { regionCache.clear() }
 }
 
 private fun validateSize(size: Int) = when {
