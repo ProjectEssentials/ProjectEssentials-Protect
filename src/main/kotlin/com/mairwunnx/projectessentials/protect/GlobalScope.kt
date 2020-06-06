be9b2558a1be59344c0e557110ed3ab121903852
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-06 19:01:52 +0300

    `asyncAsk` added to GlobalScope.kt.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/GlobalScope.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/GlobalScope.kt
@@ -2,12 +2,24 @@ package com.mairwunnx.projectessentials.protect
 
 import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI.getConfigurationByName
 import com.mairwunnx.projectessentials.protect.configurations.ProtectConfiguration
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import org.jetbrains.exposed.sql.transactions.transaction
 
 internal val configuration by lazy {
     getConfigurationByName<ProtectConfiguration>("protect")
 }
 
-internal inline fun <T> ask(crossinline body: () -> T) = transaction { body() }
+internal inline fun <T> ask(crossinline ask: () -> T) = transaction { ask() }
+
+internal inline fun <T> asyncAsk(crossinline callback: (T?) -> Unit, crossinline ask: () -> T) {
+    CoroutineScope(Dispatchers.Default).launch {
+        async {
+            ask { return@ask ask() }
+        }.await()?.let { callback(it) } ?: run { callback(null) }
+    }
+}
 
 
