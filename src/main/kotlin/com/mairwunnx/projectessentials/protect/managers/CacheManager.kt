0f9c9d5d8606ebc97af561f58b6493eb6417cc7e
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-06 13:31:13 +0300

    CacheManager.kt implemented.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/managers/CacheManager.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/managers/CacheManager.kt
@@ -1,2 +1,37 @@
 package com.mairwunnx.projectessentials.protect.managers
 
+import com.mairwunnx.projectessentials.protect.RegionPositions
+import com.mairwunnx.projectessentials.protect.configuration
+import com.mairwunnx.projectessentials.protect.dao.RegionEntity
+import com.soywiz.kds.CacheMap
+import org.apache.logging.log4j.LogManager
+
+private val logger = LogManager.getLogger()
+
+private var regionNameCache = CacheMap<String, RegionEntity>(64)
+private var regionPositionsCache = CacheMap<RegionPositions, RegionEntity>(32)
+
+fun initializeCache() = logger.debug("Initializing cache of regions").also {
+    regionNameCache = CacheMap(validateSize(configuration.take().regionNamesCacheSize))
+    regionPositionsCache = CacheMap(validateSize(configuration.take().regionPositionsCacheSize))
+}
+
+fun invalidateCache() = logger.warn("Invalidating cache of regions").also {
+    regionNameCache.clear().also { regionPositionsCache.clear() }
+}
+
+private fun validateSize(size: Int) = when {
+    size < 1 -> logger.error("Cache size can't be lower than 1!").let { 32 }
+    else -> size
+}
+
+fun cache(region: () -> RegionEntity) = cache(region())
+
+fun cache(region: RegionEntity) {
+    synchronized(regionNameCache) { regionNameCache.put(region.name, region) }
+    synchronized(regionPositionsCache) {
+        with(region) {
+            regionPositionsCache.put(RegionPositions(minX, minY, minZ, maxX, maxY, maxZ), this)
+        }
+    }
+}
