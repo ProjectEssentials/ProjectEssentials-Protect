cd1a14ef5519c4da322447f2bc0713b444770f14
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-06 13:30:30 +0300

    DatabaseManager.kt updated.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/managers/DatabaseManager.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/managers/DatabaseManager.kt
@@ -1,7 +1,7 @@
 package com.mairwunnx.projectessentials.protect.managers
 
 import com.mairwunnx.projectessentials.core.api.v1.helpers.projectConfigDirectory
-import com.mairwunnx.projectessentials.protect.entities.RegionTable
+import com.mairwunnx.projectessentials.protect.dao.RegionTable
 import net.minecraftforge.fml.server.ServerLifecycleHooks
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
