c40f933a4561c7d529e6a5860df8497779b9da42
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 18:42:03 +0300

    `EntityInteractHandler` added to activity handlers.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/handlers/ActivityHandlers.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/handlers/ActivityHandlers.kt
@@ -3,5 +3,6 @@ package com.mairwunnx.projectessentials.protect.handlers
 val activityHandlers = listOf(
     BlockBreakHandler,
     BlockPlaceHandler,
-    BlockInteractHandler
+    BlockInteractHandler,
+    EntityInteractHandler
 )
