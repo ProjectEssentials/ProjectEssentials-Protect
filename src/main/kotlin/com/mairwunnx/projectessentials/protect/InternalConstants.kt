ffa591928cb2904fa4b224a6d02eb846835ce253
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 18:18:22 +0300

    `ACTION_BLOCK_INTERACT` added to InternalConstants.kt.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/InternalConstants.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/InternalConstants.kt
@@ -2,3 +2,4 @@ package com.mairwunnx.projectessentials.protect
 
 const val ACTION_BLOCK_BREAK = "block_break"
 const val ACTION_BLOCK_PLACE = "block_place"
+const val ACTION_BLOCK_INTERACT = "block_interact"
