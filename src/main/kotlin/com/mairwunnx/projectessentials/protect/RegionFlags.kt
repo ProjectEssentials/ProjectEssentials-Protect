8102d025942088a1a4835ebca26b169e6f1daf25
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 20:44:43 +0300

    `restrict-damage` flag added.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/RegionFlags.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/RegionFlags.kt
@@ -10,6 +10,7 @@ const val FLAG_RESTRICT_UNKIND_ENTITY_SPAWN = "restrict-unkind-entity-spawn"
 const val FLAG_RESTRICT_MOD_GRIEF = "restrict-mob-grief"
 
 const val FLAG_RESTRICT_FALL_DAMAGE = "restrict-fall-damage"
+const val FLAG_RESTRICT_DAMAGE = "restrict-damage"
 
 const val FLAG_RESTRICT_FLOW_EFFECT = "restrict-flow-effect"
 const val FLAG_RESTRICT_FIRE_SPREAD = "restrict-fire-spread"
