72a0507721c04a2646700d511da729c546edbad9
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 20:23:06 +0300

    `restrict-mob-grief` flag added.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/RegionFlags.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/RegionFlags.kt
@@ -7,7 +7,9 @@ const val FLAG_ALLOW_ENTITY_INTERACT = "allow-entity-interact"
 const val FLAG_RESTRICT_ENTITY_SPAWN = "restrict-entity-spawn"
 const val FLAG_RESTRICT_KIND_ENTITY_SPAWN = "restrict-kind-entity-spawn"
 const val FLAG_RESTRICT_UNKIND_ENTITY_SPAWN = "restrict-unkind-entity-spawn"
+const val FLAG_RESTRICT_MOD_GRIEF = "restrict-mob-grief"
+
 const val FLAG_RESTRICT_FALL_DAMAGE = "restrict-fall-damage"
-const val FLAG_RESTRICT_MOD_GRIEFING = "restrict-mob-griefing"
+
 const val FLAG_RESTRICT_FLOW_EFFECT = "restrict-flow-effect"
 const val FLAG_RESTRICT_FIRE_SPREAD = "restrict-fire-spread"
