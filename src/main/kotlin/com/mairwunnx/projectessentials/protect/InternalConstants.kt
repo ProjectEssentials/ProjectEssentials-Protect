f21125681c195e3a6dac2845793538d8680b5aae
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 21:55:57 +0300

    `bucket_using` action added to InternalConstants.kt.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/InternalConstants.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/InternalConstants.kt
@@ -4,3 +4,5 @@ const val ACTION_BLOCK_BREAK = "block_break"
 const val ACTION_BLOCK_PLACE = "block_place"
 const val ACTION_BLOCK_INTERACT = "block_interact"
 const val ACTION_ENTITY_INTERACT = "entity_interact"
+const val ACTION_BUCKET_USING = "bucket_using"
+
