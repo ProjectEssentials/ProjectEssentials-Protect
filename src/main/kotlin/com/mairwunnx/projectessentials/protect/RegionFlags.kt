850f5f5663da6311b84321d6d547b32e11e42806
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 21:14:57 +0300

    `allow-farmland-damage` flag added.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/RegionFlags.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/RegionFlags.kt
@@ -3,6 +3,7 @@ package com.mairwunnx.projectessentials.protect
 const val FLAG_ALLOW_BLOCK_BREAK = "allow-block-break"
 const val FLAG_ALLOW_BLOCK_PLACE = "allow-block-place"
 const val FLAG_ALLOW_BLOCK_INTERACT = "allow-block-interact"
+const val FLAG_ALLOW_FARMLAND_DAMAGE = "allow-farmland-damage"
 const val FLAG_ALLOW_ENTITY_INTERACT = "allow-entity-interact"
 const val FLAG_RESTRICT_ENTITY_SPAWN = "restrict-entity-spawn"
 const val FLAG_RESTRICT_KIND_ENTITY_SPAWN = "restrict-kind-entity-spawn"
