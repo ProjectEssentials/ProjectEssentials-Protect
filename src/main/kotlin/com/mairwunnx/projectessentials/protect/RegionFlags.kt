1c605c4fc880a8faa442998009bcf2a717e8360f
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 17:33:49 +0300

    4 flags added in RegionFlags.kt.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/RegionFlags.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/RegionFlags.kt
@@ -2,3 +2,7 @@ package com.mairwunnx.projectessentials.protect
 
 const val FLAG_ALLOW_BLOCK_BREAK = "allow-block-break"
 const val FLAG_ALLOW_BLOCK_PLACE = "allow-block-place"
+const val FLAG_RESTRICT_FALL_DAMAGE = "restrict-fall-damage"
+const val FLAG_RESTRICT_MOD_GRIEFING = "restrict-mob-griefing"
+const val FLAG_RESTRICT_FLOW_EFFECT = "restrict-flow-effect"
+const val FLAG_RESTRICT_FIRE_SPREAD = "restrict-fire-spread"
