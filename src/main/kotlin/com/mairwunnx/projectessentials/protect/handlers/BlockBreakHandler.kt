d40e2b9093ee78272b431675b9c57249ba9e2f86
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 17:36:18 +0300

    BlockBreakHandler.kt improved.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/handlers/BlockBreakHandler.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/handlers/BlockBreakHandler.kt
@@ -2,12 +2,9 @@ package com.mairwunnx.projectessentials.protect.handlers
 
 import com.mairwunnx.projectessentials.core.api.v1.extensions.currentDimensionId
 import com.mairwunnx.projectessentials.core.api.v1.permissions.hasPermission
-import com.mairwunnx.projectessentials.protect.ACTION_BLOCK_BREAK
-import com.mairwunnx.projectessentials.protect.FLAG_ALLOW_BLOCK_BREAK
+import com.mairwunnx.projectessentials.protect.*
 import com.mairwunnx.projectessentials.protect.managers.getLastRegionAtPos
 import com.mairwunnx.projectessentials.protect.managers.getRegionFlags
-import com.mairwunnx.projectessentials.protect.participantsAsMap
-import com.mairwunnx.projectessentials.protect.restricted
 import net.minecraft.entity.player.ServerPlayerEntity
 import net.minecraftforge.common.MinecraftForge.EVENT_BUS
 import net.minecraftforge.event.world.BlockEvent
@@ -18,6 +15,7 @@ object BlockBreakHandler : ActivityHandler {
 
     @SubscribeEvent
     fun handle(event: BlockEvent.BreakEvent) {
+        if (!configuration.take().generalSettings.handleBlockBreak) return
         if (event.player !is ServerPlayerEntity) return
         val player = event.player as ServerPlayerEntity
         if (hasPermission(player, "ess.protect.bypass", 4)) return
@@ -27,6 +25,10 @@ object BlockBreakHandler : ActivityHandler {
                 player.name.string !in participantsAsMap(it.participants).keys &&
                 FLAG_ALLOW_BLOCK_BREAK !in getRegionFlags(it)
             ) restricted(player) { ACTION_BLOCK_BREAK }.also { event.isCanceled = true }
+        } ?: run {
+            if (configuration.take().globalRegionSettings.restrictBlockBreak) {
+                restricted(player) { ACTION_BLOCK_BREAK }.also { event.isCanceled = true }
+            }
         }
     }
 }
