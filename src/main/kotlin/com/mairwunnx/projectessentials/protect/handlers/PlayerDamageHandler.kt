ad5964ff166fe116b1fbe7d30882debb7cc83c88
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 20:56:25 +0300

    `PlayerDamageHandler` implemented.

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/handlers/PlayerDamageHandler.kt
@@ -0,0 +1,27 @@
+package com.mairwunnx.projectessentials.protect.handlers
+
+import com.mairwunnx.projectessentials.core.api.v1.extensions.currentDimensionId
+import com.mairwunnx.projectessentials.protect.FLAG_RESTRICT_DAMAGE
+import com.mairwunnx.projectessentials.protect.configuration
+import com.mairwunnx.projectessentials.protect.managers.getLastRegionAtPos
+import com.mairwunnx.projectessentials.protect.managers.getRegionFlags
+import net.minecraft.entity.player.ServerPlayerEntity
+import net.minecraftforge.common.MinecraftForge.EVENT_BUS
+import net.minecraftforge.event.entity.living.LivingHurtEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+
+object PlayerDamageHandler : ActivityHandler {
+    override fun init() = EVENT_BUS.register(this)
+
+    @SubscribeEvent
+    fun handle(event: LivingHurtEvent) {
+        if (!configuration.take().generalSettings.handlePlayerDamage) return
+        if (event.entity !is ServerPlayerEntity) return
+        val player = event.entity as ServerPlayerEntity
+        with(player.position) { getLastRegionAtPos(x, y, z, player.currentDimensionId) }?.also {
+            event.isCanceled = FLAG_RESTRICT_DAMAGE in getRegionFlags(it)
+        } ?: run {
+            if (configuration.take().globalRegionSettings.restrictDamage) event.isCanceled = true
+        }
+    }
+}
