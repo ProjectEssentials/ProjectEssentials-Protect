85713371a1bfd5dd7077096eb1a2595e16f67727
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-07 18:41:00 +0300

    `entity_interact` action added in InternalConstants.kt.

--- src/main/kotlin/com/mairwunnx/projectessentials/protect/InternalConstants.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/protect/InternalConstants.kt
@@ -3,3 +3,4 @@ package com.mairwunnx.projectessentials.protect
 const val ACTION_BLOCK_BREAK = "block_break"
 const val ACTION_BLOCK_PLACE = "block_place"
 const val ACTION_BLOCK_INTERACT = "block_interact"
+const val ACTION_ENTITY_INTERACT = "entity_interact"
